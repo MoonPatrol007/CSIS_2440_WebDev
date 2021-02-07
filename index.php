1f0eef06f07647a5f43408f9092e4e80f5ddf6bc
Hector Juarez <mj.hector@gmail.com>
2021-02-06 22:03:38 -0700

    Displaying the word Happy-Birthday
    In the index.php file: (Using XAMPP, PHP, HTML, CSS)
    Create a standard web page with all the basics (doctype, html tag, head tag, body, etc.)
    In the body of the page:
    Have PHP echo out the statement "Happy Birthday!" inside of a heading 1 tag.
    Also, using PHP, display an image below the message.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<title>happy-birthday Website</title>
+	</head>
+	
+	<body>
+		<?php
+			echo "<h1>Happy Birthday!</h1>";
+			
+			echo "<img src='img\scoobyDooCrew.jpg' alt='scooby doo photo'>";
+			
+		?>
+		
+	</body>
+
+</html>
